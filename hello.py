d8e982b2aa97a084597db789d7ec8c6038989326
lgm-hub <lorenzo.g.merlini@gmail.com>
2025-09-20 09:37:36 +0200

    summarized my ideas for the app as comments

--- hello.py
+++ hello.py
@@ -1,1 +1,30 @@
 print ("Hello, Coffee Tracker!")
+
+
+#nice-to-have: **xy**
+#advanced nice-to-have: ***xy***
+
+#Ideas:
+#first thing when you install the app "select your default grinder and machine" (skippable)
+#you can manage your gadgets:
+    #- you can manage your inventory of gadgets (machines, tools, consumables) 
+    #- you cancreate wishlist items (even link to websites to buy them)
+    #- you can create sets of grinder + machine (espresso machine + basket etc) or moka, french press etc
+    #- alert for when wishlisted items are on sale?
+#you can manage your coffee inventory:
+    #- save coffees that you (track all info that you have on it)
+    #- rate coffees
+    #- wishlist coffees
+    #- you can save recipes for each coffee by combining them with your sets
+    #- *** roaster around feature: see what coffee roaster are in your area ***
+#you can then track each shot or brew: 
+    #- assign a set and recipe, make small adjustments on the fly
+        #- they don't overwrite your used recipe
+        #- option to overwrite recipe, create new recipe or discard (default)
+    #- give ratings and take notes
+    #- **connect your analytics devices to automatically import all shot statistics**
+    #- *** there is a guide to help you rate each shot ***
+#smart features and analytics:
+    #- you can see statistics for each brew type, set and coffee and document your findings
+    #- **there are smart analytics that recommend you recipes for your coffees**
+    #- ***based on your ratings, favourite sets and coffee types, you are recommended coffee from local coffee roasters***
