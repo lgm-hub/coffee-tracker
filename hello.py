3d6dae4d084b608b340cda0000e2dfedd5959a90
lgm-hub <lorenzo.g.merlini@gmail.com>
2025-09-23 21:46:45 +0200

    planned more features

--- hello.py
+++ hello.py
@@ -18,11 +18,11 @@ print ("Hello, Coffee Tracker!")
     #- you can create sets of grinder + machine (espresso machine + basket etc) or moka, french press etc
     #- alert for when wishlisted items are on sale?
 #you can manage your coffee inventory:
-    #- save coffees that you (track all info that you have on it)
+    #- save coffees that you have (track all info that you have on it)
+        #- ***AI scan to add info automatically***
     #- rate coffees
     #- wishlist coffees
     #- you can save recipes for each coffee by combining them with your sets
-    #- *** roaster around feature: see what coffee roaster are in your area ***
 #you can then track each shot or brew: 
     #- assign a set and recipe, make small adjustments on the fly
         #- they don't overwrite your used recipe
@@ -30,7 +30,15 @@ print ("Hello, Coffee Tracker!")
     #- give ratings and take notes
     #- **connect your analytics devices to automatically import all shot statistics**
     #- *** there is a guide to help you rate each shot ***
-#smart features and analytics:
+#**smart features and analytics:**
     #- you can see statistics for each brew type, set and coffee and document your findings
     #- **there are smart analytics that recommend you recipes for your coffees**
     #- ***based on your ratings, favourite sets and coffee types, you are recommended coffee from local coffee roasters***
+#***community features:***
+    #- roaster around feature: see what coffee roaster are in your area
+        # for this, API to apps like "Roasters"
+    #- blog feature?
+        #- local discussions
+        #- coffee/roaster discussion
+        #- equipment discussion
+    #- events
