c05475f2755c6995581fafa12db79d8272b3b8e5
lgm-hub <lorenzo.g.merlini@gmail.com>
2025-09-26 21:10:49 +0200

    added new file to start experimenting with coding

---
+++ start.py
@@ -0,0 +1,9 @@
+#here I will start to experiment with the code
+
+#defining a list for coffee (will be an object in the future)
+list_coffee_names = ["Suncita -GOTA", "Karmeliter Mischung - Alt Wien", "Ciao Bello - Alt Wien"]
+
+#trying methods
+list_coffee_names.append("Forest Cake - Mistral")
+
+print(list_coffee_names)
